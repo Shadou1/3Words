58570b30079329cdc32988194595fbf032f4e30e
Sha Dou <shadou777@gmail.com>
2020-09-13 18:13:30 +0700

    Add language to word.html

--- bin/parsers/wiktionary_parser_soup.py
+++ bin/parsers/wiktionary_parser_soup.py
@@ -4,7 +4,7 @@ from urllib.error import HTTPError
 
 from bs4 import BeautifulSoup
 
-def get_wiktionary_word(word):
+def get_wiktionary_word(word, english_only=True):
 
     try:
 
@@ -17,7 +17,7 @@ def get_wiktionary_word(word):
 
     soup = BeautifulSoup(html, 'html.parser')
 
-    word_dictionary = parse_dictionary(soup)
+    word_dictionary = parse_dictionary(soup, english_only)
 
     return word_dictionary
 
@@ -26,10 +26,12 @@ def parse_dictionary(soup: BeautifulSoup, english_only=True):
     dictionary = {}
 
     re_etymology = re.compile(r'Etymology.*')
-    re_part_of_speech = re.compile(r'Noun|Verb|Adjective|Adverb')
-
+    re_part_of_speech = re.compile(r'Noun|Verb|Adjective|Adverb|' +
+                                r'Numeral|Pronoun|Conjunction|Preposition|Interjection|Romanization')
 
     # english = soup.find('span', {'id': 'English'})
+
+    all_elements = soup.find_all(True)
 
     h2_list = soup.findAll(lambda elem: elem.name == 'h2' and elem.string != 'Contents' and elem.string != 'Navigation menu')
     languages_list = [lang.find('span') for lang in h2_list]
@@ -38,13 +40,42 @@ def parse_dictionary(soup: BeautifulSoup, english_only=True):
     descriptions_list = soup.findAll('ol')
     translations_list = soup.findAll('table', class_='translations')
 
+    # TODO Optimize the adding of etymology
+
+    lang_range = 1 if english_only and len(languages_list) > 1 else len(languages_list) - 1
+    for i in range(lang_range):
+        for etymology in etymology_list:
+            if etymology in all_elements[
+                all_elements.index(languages_list[i]):
+                all_elements.index(languages_list[i + 1])]:
+                # print('Etymology in', languages_list[i]['id'])
+                break
+        else:
+            # print('No etymology in', languages_list[i]['id'])
+            someEtymology = soup.new_tag('span', id='Etymology')
+            languages_list[i].insert_after(someEtymology)
+            etymology_list.append(someEtymology)
+
+    if not english_only:
+        for etymology in etymology_list:
+            if etymology in all_elements[all_elements.index(languages_list[-1]):]:
+                # print('Etymology in', languages_list[-1]['id'])
+                break
+        else:
+            # print('No etymology in', languages_list[-1]['id'])
+            someEtymology = soup.new_tag('span', id='Etymology')
+            languages_list[i].insert_after(someEtymology)
+            etymology_list.append(someEtymology)
+
     # Find Language
     for language in languages_list:
         if english_only and language['id'] != "English": continue
 
         dictionary[language['id']] = {}
 
-        # TODO Will not parse language descriptions if it does not have Etymology
+        # Add etymology if etymology is not found
+        # print(all_elements.index(language))
+
         # Find Etymology
         for next_etymology in language.next_elements:
             if next_etymology in etymology_list:
@@ -84,7 +115,9 @@ def parse_dictionary(soup: BeautifulSoup, english_only=True):
                                     # Add the rest of the text as a description
                                     desc_text = desc.get_text().strip()
 
-                                    dictionary[language['id']][next_etymology['id']][next_part_of_speech['id']].append([desc_text, examples])
+                                    # TODO find out while somethimes desc_text is empty
+                                    if desc_text:
+                                        dictionary[language['id']][next_etymology['id']][next_part_of_speech['id']].append([desc_text, examples])
 
                                 break
 
@@ -107,10 +140,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("word", help="word from wiktionary", default='staple')
     parser.add_argument("--file", help="json file to write to")
+    parser.add_argument("--english-only", help="only search for English definitions", default='yes')
     args = parser.parse_args()
     if not args.file: args.file = args.word + '.json'
 
-    word_dictionary = get_wiktionary_word(args.word)
+    word_dictionary = get_wiktionary_word(args.word, True if args.english_only == 'yes' else False)
 
     with open(args.file, 'w', encoding='utf-8') as f:
         json.dump(word_dictionary, f, indent=2, ensure_ascii=False)
